21a837ebdff56b682a4396cdb87e8a28eb3c69db
Meek Msaki <98189596+mmsaki@users.noreply.github.com>
2025-03-24 12:29:25 -0500

    feat: add dashboard cards

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,13 +1,17 @@
+import { DashboardCards } from "./dashboard-cards";
+
 export default function Dashboard() {
 	return (
 		<>
 			<h2>Dashboard</h2>
-			<div className="grid auto-rows-min gap-4 md:grid-cols-3">
-				<div className="aspect-square rounded-xl bg-muted/50" />
-				<div className="aspect-square rounded-xl bg-muted/50" />
-				<div className="aspect-square rounded-xl bg-muted/50" />
+			<div className="flex flex-1 flex-col">
+				<div className="@container/main flex flex-1 flex-col">
+					<div className="flex flex-col md:gap-6 md:py-6">
+						<DashboardCards />
+					</div>
+					<div className="min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min"></div>
+				</div>
 			</div>
-			<div className="min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min" />
 		</>
 	);
 }
