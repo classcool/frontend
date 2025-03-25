3eb5591ab78a2f848e8e933686003c54a7553955
Meek Msaki <98189596+mmsaki@users.noreply.github.com>
2025-03-25 13:11:10 -0500

    chore: remove gradient

--- src/app/dashboard/dashboard-cards.tsx
+++ src/app/dashboard/dashboard-cards.tsx
@@ -72,7 +72,7 @@ export function DashboardCards() {
 	const loading = <Skeleton className="aspect-video rounded-xl" />;
 
 	return (
-		<div className="*:data-[slot=card]:shadow-xs @xl/main:grid-cols-2 @5xl/main:grid-cols-3 grid grid-cols-1 gap-4 *:data-[slot=card]:bg-gradient-to-t *:data-[slot=card]:from-primary/5 *:data-[slot=card]:to-card dark:*:data-[slot=card]:bg-card">
+		<div className="*:data-[slot=card]:shadow-xs @xl/main:grid-cols-2 @5xl/main:grid-cols-3 grid grid-cols-1 gap-4 *:data-[slot=card]:from-primary/5 *:data-[slot=card]:to-card dark:*:data-[slot=card]:bg-card">
 			{hooksData.isLoading || hooksData.isFetching
 				? loading
 				: TotalCountQuery({
