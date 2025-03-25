89ad6a536fdb966b9d51361d104b525d14104c0f
Meek Msaki <98189596+mmsaki@users.noreply.github.com>
2025-03-25 13:16:17 -0500

    feat: remove url endpoint log

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -14,7 +14,6 @@ export async function POST(request: NextRequest) {
 		);
 	}
 
-	console.log("endpoint 🦉", endpoint);
 	const variables = new Object() as {
 		cursor: string;
 		chainId: number | string;
